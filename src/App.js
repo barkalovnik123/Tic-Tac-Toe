bdf86ed83be2173a7a6f94a57f06a23d0d9d3ee2
barkalovnik123 <69666502+barkalovnik123@users.noreply.github.com>
2025-02-14 21:16:23 +0700

    Did some styling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react';
-import {calculateWinners} from "./worker";
+import './App.scss';                        //Импорт стилей
+import {useState} from 'react';             //Импорт useState
+import {calculateWinners} from "./worker";  //Импорт вспомогательных функций
 
-const Square = ({value, onSquareClick}) => {
-    return <button className="square" onClick={onSquareClick}>{value}</button>;
+const Square = ({value, onSquareClick}) => {//Клеточка, куда ставят X или O
+    const cls = "square " + (value === "X" ? "x-square" : "o-square");  //Класс
+    /* Засчёт изменения класса меняются некоторые css-свойства */
+    return <div className={cls} onClick={onSquareClick}>{value}</div>;
 }
 
-const Board = () => {
-    const [turn, setTurn] = useState(true);
+const Board = () => {   //Доска, хранит все необходимые state
+    const [turn, setTurn] = useState(true); //Чей сейчас ход
     const [squares, setSquares] = useState(Array(9).fill(null));
+    /* squares - массив, в котором хранится состояние поля */
     const [history, setHistory] = useState(Array(0));
-
-    function handleClick(i) {
-        const nextSquares = squares.slice();
+    /* Каждый член history - массив squares в момент времени в прошлом */
+    function handleClick(i) {   //
+        const nextSquares = squares.slice();    // Берём срез-копию
         setHistory(history.slice().concat([nextSquares])); // Вставляет в историю игры новый массив
-        nextSquares[i] = turn ? "X" : "O"; // Выбирает между плюсом и минусом
-        if (calculateWinners(nextSquares)) {
-            alert((turn ? "X" : "O") + " wins!");
+        nextSquares[i] = turn ? "X" : "O";   // Выбирает между плюсом и минусом
+        if (calculateWinners(nextSquares)) { //Проверяем, не выйгрышная ли ситуация на поле
+            alert((turn ? "X" : "O") + " wins!");                    // Выводим сообщение о победе
+            setSquares(Array(9).slice().fill(null));//
+            setTurn(true);  //Устанавливаем снова ход X
+            return;
         }
         setTurn(!turn); //Меняет чей сейчас ход
         setSquares(nextSquares);
     }
 
     const domSquares = Array(9).fill(null)
-        .map((e, i) => <Square value={squares[i]} onSquareClick={() => handleClick(i)}/>);
+        .map((e, i) => <Square
+            key={Math.random()}
+            value={squares[i]}
+            onSquareClick={() => handleClick(i)}/>);
 
     return (<div className={"board"}>
         {domSquares}
@@ -34,7 +42,9 @@ const Board = () => {
 const App = () => {
     return (
         <div className="App">
+            <div className={"board__wrapper"}>
             <Board></Board>
+            </div>
         </div>
     );
 }
